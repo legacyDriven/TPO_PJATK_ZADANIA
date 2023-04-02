26920b43bcecea350f7ff71d67854354ab8938d9
Eugeniusz <eugenioskye@gmail.com>
2023-04-02 04:12:56 +0200

    tpo1 up and running

--- TPO1_SE_S23951/src/zad2/Main.java
+++ TPO1_SE_S23951/src/zad2/Main.java
@@ -9,11 +9,12 @@ package zad2;
 
 public class Main {
   public static void main(String[] args) {
-    Service s = new Service("Poland");
-    String weatherJson = s.getWeather("Warsaw");
-    Double rate1 = s.getRateFor("USD");
-    Double rate2 = s.getNBPRate();
+//    Service s = new Service("Poland");
+//    String weatherJson = s.getWeather("Warsaw");
+//    Double rate1 = s.getRateFor("USD");
+//    Double rate2 = s.getNBPRate();
     // ...
     // część uruchamiająca GUI
+    new View().init();
   }
 }
